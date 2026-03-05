da20c275dcbd34c8199c0cb3a0b04014a7fb3527
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-03-06 00:10:48 +0600

    Add files via upload

---
+++ matrix_problem.cpp/Matrix_addition.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c, A[10][10], B[10][10], C[10][10];
+
+    cin >> r >> c;
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> A[i][j];
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> B[i][j];
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            C[i][j] = A[i][j] + B[i][j];
+
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++)
+            cout << C[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
