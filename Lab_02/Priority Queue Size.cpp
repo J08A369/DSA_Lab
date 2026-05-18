8a8cc3af013ebb670e3f9fb7bb29dd1af9dbef04
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-18 21:38:25 +0600

    Add files via upload

---
+++ Lab_02/Priority Queue Size.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int> pq;
+
+    pq.push(5);
+    pq.push(15);
+    pq.push(25);
+
+    cout << pq.size() << endl;
+}
