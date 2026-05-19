e60cd006eeb96e15aed82aeb7b9a98cd339b004d
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-19 09:01:32 +0600

    Add files via upload

---
+++ Lab_02/Simple Priority Queue.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int> pq;
+
+    pq.push(30);
+    pq.push(10);
+    pq.push(50);
+    pq.push(20);
+
+    while(!pq.empty())
+    {
+        cout << pq.top() << endl;
+        pq.pop();
+    }
+}
