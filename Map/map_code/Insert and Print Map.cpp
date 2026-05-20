65d333fb636c851396b3086fdd89095959575af5
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-20 21:39:05 +0600

    Add files via upload

---
+++ Map/map_code/Insert and Print Map.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string,int> student;
+
+    student["mina"] = 10;
+    student["tina"] = 20;
+    student["sina"] = 30;
+
+    for(auto x : student)
+    {
+        cout << x.first << " " << x.second << endl;
+    }
+}
