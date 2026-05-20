15691ce2d0ecdd3495f6d9d5494dcaa452b4a320
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-20 21:42:31 +0600

    Add files via upload

---
+++ Map/map_code/Update Value in Map.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string,int> student;
+
+    student["S"] = 40;
+
+    cout << student["S"] << endl;
+
+    student["S"] = 100;
+
+    cout << student["S"] << endl;
+}
